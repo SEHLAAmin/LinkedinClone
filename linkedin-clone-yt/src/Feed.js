2e3eb101490fabe7244787446164491aa2f57a28
Amin <amin.sla2015@gmail.com>
2021-08-02 02:06:04 +0200

    ajout fonctionnalite post dans le feed avec firebase

--- linkedin-clone-yt/src/Feed.js
+++ linkedin-clone-yt/src/Feed.js
@@ -1,4 +1,4 @@
-import React, {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import CreateIcon from "@material-ui/icons/Create";
 import "./Feed.css";
 import InputOption from "./InputOption";
@@ -8,71 +8,104 @@ import EventNoteIcon from "@material-ui/icons/Event"
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay"
 import Post from "./Post";
 import { db } from "./firebase";
+import firebase from 'firebase'
 
 
 
 function Feed() {
 
-const [input , setInput] = useState('')
+    const [input, setInput] = useState('')
 
-const onChangeHandler = event => {
-  setInput(event.target.value);
-};
-
-
-const [posts, setPosts] = useState([])
-
-console.log(input);
+    const onChangeHandler = event => {
+        setInput(event.target.value);
+    };
 
 
-useEffect(()=> {
-db.collection("posts").onSnapshot( snapshot => (
-  setPosts(snapshot.docs.map(doc => 
-    (
-      {
-        id : doc.id , 
-        data : doc.data(),
-      }
-    )))
-))
-}, [])
+    const [posts, setPosts] = useState([])
+
+    console.log(input);
 
 
+    useEffect(() => {
+        db.collection("posts").onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc =>
+                ({
+                    id: doc.id,
+                    data: doc.data(),
+                })))
+        ))
+    }, [])
 
-const sendPost = e => {
-  e.preventdefault();
-  db.collection('posts').add({
-    name : 'Amin',
-    description : 'test',
-    message : input
-  })
-}
-  return (
-    <div className="feed">
-      <div className="feed_inputContainer">
-        <div className="feed_input">
-          <CreateIcon />
-          <form action="">
-            <input value={input} onChange={onChangeHandler}    type="text" />
-            <button type="submit">send</button>
-          </form>
-        </div>
-        <div className="feed_inputOptions">
-            <InputOption Icon = {ImageIcon}  title="Photo" color="#70B5F9"/>
-            <InputOption Icon = {SubscriptionsIcon}  title="Video" color="#E7A33E"/>
-            <InputOption Icon = {EventNoteIcon}  title="Event" color="#C0CBCD"/>
-            <InputOption Icon = {CalendarViewDayIcon}  title="Whrite article" color="#7FC15E"/>
+    const sendPost = (e) => {
+
+        e.preventDefault();
+        db.collection("posts").add({
+            name: "amien",
+            description: "test",
+            message: input,
+            photoUrl: "",
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
 
 
-        </div>
-      </div>
-     { posts.map((post)=> 
-     <Post />
-     )}
-         <Post  name ='Sehla' description='test' message ='waow this work ' /> 
+        })
+    }
 
-    </div>
-  );
+
+    return ( <
+        div className = "feed" >
+        <
+        div className = "feed_inputContainer" >
+        <
+        div className = "feed_input" >
+        <
+        CreateIcon / >
+        <
+        form action = "" >
+        <
+        input value = { input }
+        onChange = { onChangeHandler }
+        type = "text" / >
+        <
+        button onClick = { sendPost }
+        type = "submit" > send < /button> <
+        /form> <
+        /div> <
+        div className = "feed_inputOptions" >
+        <
+        InputOption Icon = { ImageIcon }
+        title = "Photo"
+        color = "#70B5F9" / >
+        <
+        InputOption Icon = { SubscriptionsIcon }
+        title = "Video"
+        color = "#E7A33E" / >
+        <
+        InputOption Icon = { EventNoteIcon }
+        title = "Event"
+        color = "#C0CBCD" / >
+        <
+        InputOption Icon = { CalendarViewDayIcon }
+        title = "Whrite article"
+        color = "#7FC15E" / >
+
+
+        <
+        /div> <
+        /div> {
+            posts.map(({ id, data: { name, message, descritpion, photoUrl } }) =>
+                <
+                Post key = { id }
+                name = { name }
+                description = { descritpion }
+                message = { message }
+                photoUrl = { photoUrl }
+                />
+            )
+        }
+
+        <
+        /div>
+    );
 }
 
 export default Feed;
